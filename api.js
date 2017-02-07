37d1470904443ea06cffed481840890aae6778b3
kainsavage <kainsavage@yahoo.com>
2017-02-07 11:38:36 -0800

    Comments

--- api.js
+++ api.js
@@ -8,6 +8,15 @@ class API extends ExtensionAPI {
   getAPI(context) {
     return {
       moresessions: {
+        /**
+         * Attempts to forget a recently closed tab, identified by `sessionId`
+         * from the given window.
+         *
+         * @param {Integer} windowId The identifier of the window to which the
+         *                  tab to be forgotten belongs.
+         * @param {Integer} sessionId The sessionId (closedId) of the tab to be
+         *                  forgotten.
+         */
         async forgetClosedTab(windowId,sessionId) {
           let aWindow = context.extension.windowManager.get(windowId).window;
           let closedTabData = JSON.parse(sessionStore.getClosedTabData(aWindow));
@@ -19,6 +28,12 @@ class API extends ExtensionAPI {
           sessionStore.forgetClosedTab(aWindow, closedTabIndex);
         },
 
+        /**
+         * Attempts to forget a recently closed window.
+         *
+         * @param {Integer} sessionId The sessionId (closedId) of the window to
+         *                  be forgotten.
+         */
         async forgetClosedWindow(sessionId) {
           let closedWindowData = JSON.parse(sessionStore.getClosedWindowData());
 
