e2826d93afbf450bc9c65e3f747ce931fa52bea1
kainsavage <kainsavage@yahoo.com>
2017-02-07 08:41:45 -0800

    Fixes a bug with tabIndex

--- api.js
+++ api.js
@@ -3,8 +3,6 @@ const CI = Components.interfaces;
 
 const sessionStore   = CC["@mozilla.org/browser/sessionstore;1"]
                          .getService(CI.nsISessionStore);
-const windowMediator = CC["@mozilla.org/appshell/window-mediator;1"]
-                         .getService(CI.nsIWindowMediator);
 
 class API extends ExtensionAPI {
   getAPI(context) {
